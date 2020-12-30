a8e94a2b19f61bb5f47b119103274b0f7664c1ee
Abhishek Doshi <adoshi26.ad@gmail.com>
2020-12-30 14:07:23 +0530

    refactired number_trivia_repository

--- lib/features/number_trivia/domain/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:number_trivia_tdd_architecture/core/error/exceptions.dart';
 import 'package:number_trivia_tdd_architecture/core/platform/network_info.dart';
 import 'package:number_trivia_tdd_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number_trivia_tdd_architecture/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
@@ -18,14 +19,31 @@ class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   });
   @override
   Future<Either<Failure, NumberTrivia>> getConcrete(int number) async {
-    networkInfo.isConnected;
-    final remoteTrivia = await remoteDataSource.getConcreteTrivia(number);
-    localDataSource.cacheNumberTrivia(remoteTrivia);
-    return Right(remoteTrivia);
+    return await _getTrivia(() => remoteDataSource.getConcreteTrivia(number));
   }
 
   @override
-  Future<Either<Failure, NumberTrivia>> getRandom() {
-    return null;
+  Future<Either<Failure, NumberTrivia>> getRandom() async {
+    return await _getTrivia(() => remoteDataSource.getRandomTiriva());
+  }
+
+  Future<Either<Failure, NumberTrivia>> _getTrivia(
+      Future<NumberTrivia> Function() getConcreteOrRandom) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteTrivia = await getConcreteOrRandom();
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 }
