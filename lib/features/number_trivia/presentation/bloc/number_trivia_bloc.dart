de1a0ca7e4f666eeba4a9e60455c826fc6bcdbdd
Abhishek Doshi <adoshi26.ad@gmail.com>
2020-12-31 13:46:03 +0530

    bloc implemented

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:number_trivia_tdd_architecture/core/error/failure.dart';
+import 'package:number_trivia_tdd_architecture/core/usecases/usecase.dart';
 import 'package:number_trivia_tdd_architecture/core/util/input_converter.dart';
 import 'package:number_trivia_tdd_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_tdd_architecture/features/number_trivia/domain/usecases/get_concrete.dart';
@@ -34,8 +36,37 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
           yield NumberTriviaErrorState(
               errorMessage: Invalid_Input_Failure_Message);
         },
-        (integer) => throw UnimplementedError(),
+        (integer) async* {
+          yield NumberTriviaLoadingState();
+          final trivia = await getConcrete(Params(number: integer));
+          yield trivia.fold(
+            (failure) => NumberTriviaErrorState(
+              errorMessage: _mapFailureToMessage(failure),
+            ),
+            (trivia) => NumberTriviaLoadedState(trivia: trivia),
+          );
+        },
       );
+    } else if (event is GetTriviaForRandom) {
+      yield NumberTriviaLoadingState();
+      final trivia = await getRandom(NoParams());
+      yield trivia.fold(
+        (failure) => NumberTriviaErrorState(
+          errorMessage: _mapFailureToMessage(failure),
+        ),
+        (trivia) => NumberTriviaLoadedState(trivia: trivia),
+      );
+    }
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return Server_Failure_Message;
+      case CacheFailure:
+        return Cache_Failure_Message;
+      default:
+        return 'Unexpected Error!!';
     }
   }
 }
